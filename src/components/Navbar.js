2d2426e9c421328e3a2e4181f5d4393aa113c32a
vaishnavicharkha25 <vaishnavicharkha25@gmail.com>
2024-07-10 18:22:30 +0530

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,23 +5,26 @@ const NavBar = () => {
   return (
     <div className='sticky-top'>
       <nav className="sticky-top navbar navbar-expand-lg bg-purple">
-        <div className='nav-brand'><h3 className='text-white pt-1 px-5'>Student management</h3></div>
-        <ul className="navbar-nav ">
-          <li className="nav-item pt-1">
-            <Link className="nav-link text-white px-4 text-16" to="/">Dashboard</Link>
-          </li>
-          <li className="nav-item  pt-1">
-            <Link className="nav-link text-white px-4 text-16" to="/students/deactivated">Deactivated Students</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link px-4" to="/add-student"><button className='btn btn-add' type="submit">
-              Add Student
-            </button></Link>
-          </li>
-        </ul>
+        <div className='nav-brand'>
+          <h3 className='text-white pt-1 px-4'>Student Management</h3>
+        </div>
+        <div className="collapse navbar-collapse justify-content-end">
+          <ul className="navbar-nav">
+            <li className="nav-item pt-1">
+              <Link className="nav-link text-white px-4 text-16" to="/">Dashboard</Link>
+            </li>
+            <li className="nav-item pt-1">
+              <Link className="nav-link text-white px-4 text-16" to="/students/deactivated">Deactivated Students</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link px-4" to="/add-student">
+                <button className='btn btn-add' type="submit">Add Student</button>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     </div>
-
   );
 };
 
